4ea58d6afc8b3a779b35107903ae822bb125357a
Denis Christopher <la171126@hotmail.com>
2019-07-03 11:54:02 +0200

    search (show on click)

--- index.php
+++ index.php
@@ -1,43 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Fris</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
-        <link href="toastr.css" rel="stylesheet"/>
-    </head>
-    <body>
-        <div id="form" data-callback="process.php">
-            <label for="search">Search:</label>
-            <input type="text" name="search" id="search" placeholder="Search..">
-        </div>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Fris</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+    <link href="toastr.css" rel="stylesheet"/>
+</head>
+<body>
+<!-- FORM -->
+<form action="" method="post" data-callback="process.php">
+    <label for="search">Search:</label>
+    <input type="text" name="search" id="search" placeholder="Search..">
+    <select name="lang">
+        <option value="nl">Nederlands</option>
+        <option value="en">English</option>
+    </select>
+    <input type="submit" value="submit">
+    <div id="notification"></div>
+</form>
 
-        <h4 style="margin-bottom: 5px">Result:</h4>
-        <span id="result"></span>
-        <!-- SCRIPTS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-        <script>
-            search.oninput = function() {
-                if($("#form").attr("data-callback") != null) {
-                    var formData = new FormData();
-                    formData.append("search", search.value);
+<!-- SCRIPTS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+<script>
+    /* ------------------------------------------------------
+                            FORM SENDING
+    ------------------------------------------------------ */
+    $(document).ready(function () {
+        //Form auto sending via ajax
+        $(document).on("submit", "form", function (e) {
+            if($(this).attr("data-callback") != null) {
+                var form = $(this).get(0)
+                var formData = new FormData(form)
 
-                    $url = $('#form').attr("data-callback");
+                $url = $(this).attr("data-callback")
 
-                    $.ajax({
-                        url: $url,
-                        type: "POST",
-                        data: formData,
-                        processData: false,
-                        contentType: false,
-                        complete: function (data) {
-                            $("#result").html(data.responseText);
-                        }
-                    });
-                }
-            };
-        </script>
-    </body>
+                e.preventDefault();
+
+                $.ajax({
+                    url: $url,
+                    type: "POST",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    complete: function (data) {
+                        $("#notification").html(data.responseText); // .html pour réécrire par dessus
+                    }
+                });
+            }
+        });
+    });
+</script>
+</body>
 </html>
--- process.php
+++ process.php
@@ -4,9 +4,9 @@
 
 
     /* MAIN CODE */
-    if(isset($_POST['search']) && !empty($_POST['search'])) {
+    if(isset($_POST['search']) && !empty($_POST['search']) && isset($_POST['lang']) && !empty($_POST['lang'])) {
         $search = str_replace(' ', '%20', htmlspecialchars($_POST['search']));
-        $langWebsite = 'en';
+        $langWebsite = htmlspecialchars($_POST['lang']);
 
         if ($langWebsite == 'en') {
             $wordList = getWordList($search);
